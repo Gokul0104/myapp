268774399b607539e412feb759b314e1eea19bb5
Gokul <gokul.v@niveussolutions.com>
2022-09-21 19:04:26 +0530

    ASSIGNMENT 2

---
+++ app.js
@@ -0,0 +1,52 @@
+
+const { response } = require('express');
+const express = require('express');
+const bodyparser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 8080;
+app.use(bodyparser.urlencoded({ extended: true }))
+
+const myObj = {
+    userss: [
+        {
+            id: 1,
+            name: "Gokul"
+        },
+        {
+            id: 2,
+            name: "Shivu"
+        },
+        {
+            id: 3,
+            name: "Dilip"
+        },
+
+
+    ]
+}
+
+console.log(myObj);
+console.log(myObj.userss.some((ele)=>ele.id == 1));
+app.get('/userss/:id',(req,res)=>{
+    console.log(req.params);
+    let myJson =[];
+    if(myObj.userss.some((ele)=>ele.id == req.params.id)){
+        myJson = myObj.userss.filter((ele)=>ele.id == req.params.id)
+    }
+    res.json(myJson);
+})
+
+
+app.get('/user', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/users', (req, res) => {
+    console.clear();
+    console.log(req.body);
+    res.send(`DONE:User ${req.body.user} ID ${req.body.id}`);
+})
+
+app.listen(port, () => {
+    console.log(`Ready Listening on ${port}`);
+})
