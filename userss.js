703d73603db39d2a4d0aac74c6cd8209a6f91de3
Gokul <gokul.v@niveussolutions.com>
2022-09-22 11:04:14 +0530

    Changes done

---
+++ userss.js
@@ -0,0 +1,20 @@
+const myObj = {
+    userss: [
+        {
+            id: 1,
+            name: "Gokul"
+        },
+        {
+            id: 2,
+            name: "Shivu"
+        },
+        {
+            id: 3,
+            name: "Dilip"
+        },
+
+
+    ]
+}
+
+module.exports = myObj;
