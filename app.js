f8686ca34f5add952f29c708886b13a674865a63
Gokul <gokul.v@niveussolutions.com>
2022-09-22 11:03:10 +0530

    Changes

--- app.js
+++ app.js
@@ -2,36 +2,20 @@
 const { response } = require('express');
 const express = require('express');
 const bodyparser = require('body-parser');
+const myObj = require('./userss');
 const app = express();
 const port = process.env.PORT || 8080;
-app.use(bodyparser.urlencoded({ extended: true }))
-
-const myObj = {
-    userss: [
-        {
-            id: 1,
-            name: "Gokul"
-        },
-        {
-            id: 2,
-            name: "Shivu"
-        },
-        {
-            id: 3,
-            name: "Dilip"
-        },
-
-
-    ]
-}
+app.use(bodyparser.urlencoded({
+    extended: true
+}))
 
 console.log(myObj);
-console.log(myObj.userss.some((ele)=>ele.id == 1));
-app.get('/userss/:id',(req,res)=>{
+console.log(myObj.userss.some((ele) => ele.id == 1));
+app.get('/userss/:id', (req, res) => {
     console.log(req.params);
-    let myJson =[];
-    if(myObj.userss.some((ele)=>ele.id == req.params.id)){
-        myJson = myObj.userss.filter((ele)=>ele.id == req.params.id)
+    let myJson = [];
+    if (myObj.userss.some((ele) => ele.id === parseInt(req.params.id))) {
+        myJson = myObj.userss.filter((ele) => ele.id === parseInt(req.params.id))
     }
     res.json(myJson);
 })
